1c2cb1501337344c4301b5dc5517cfe5aa0104d9
David <cmc_weiyg@163.com>
2020-06-24 04:33:24 +0800

    WordCloud

---
+++ Experiment/WordCloud/WordCloud1.py
@@ -0,0 +1,6 @@
+# 1号词云：《葛底斯堡演说》黑色背景词云（4行代码上手）
+
+import wordcloud
+w = wordcloud.WordCloud()
+w.generate('and that government of the people, by the people, for the people, shall not perish from the earth.')
+w.to_file('WordCloud/utput1.png')
---
+++ Experiment/WordCloud/readme.md
@@ -0,0 +1,5 @@
+https://github.com/TommyZihao/zihaowordcloud
+
+ref：https://www.cnblogs.com/wkfvawl/p/11585986.html
+
+安装包：pip install numpy matplotlib pillow wordcloud imageio jieba snownlp itchat -i https://pypi.tuna.tsinghua.edu.cn/simple
