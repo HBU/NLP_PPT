e0eb4becc1e3db195806265bf1284e945242ca3c
David <cmc_weiyg@163.com>
2020-06-25 03:54:31 +0800

    1

--- Experiment/ChineseWordSegmentation/Test04_NLTK.py
+++ Experiment/ChineseWordSegmentation/Test04_NLTK.py
@@ -1,9 +1,21 @@
 import nltk
-nltk.download('punkt')
-#解析中文
-#做中文分词解析，分割符一定要用“.”才可以正确识别解析（“.”后面一定要一个空格）
+#nltk.set_proxy('http://proxy.example.com:3128', ('USERNAME', 'PASSWORD'))
+#nltk.download()
+#nltk.download('punkt')  又是一个大坑~ 这里怎么弄，都不能用。 慢慢总结吧。 学新东西是真不容易 2020.6.25 David
+nltk.data.path.append("C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python38\\Lib\\nltk_data")
+#先分句再分词
+sents = nltk.sent_tokenize("And now for something completely different. I love you.")
+word = []
+for sent in sents:
+    word.append(nltk.word_tokenize(sent))
+print(word)
 
-text1 = '同是风华正茂，怎敢甘拜下风 . 保持学习，保持饥饿'
-text2 = 'Hello World'
-Juzi_chinese = nltk.sent_tokenize(text2)
-print(Juzi_chinese)
+#分词
+text = nltk.word_tokenize("And now for something completely different.")
+print(text)
+#词性标注
+tagged = nltk.pos_tag(text)
+print (tagged[0:6])
+#命名实体识别
+entities = nltk.chunk.ne_chunk(tagged)
+print (entities)
